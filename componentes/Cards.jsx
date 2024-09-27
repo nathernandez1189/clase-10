ae2b235084058832b3fe5493fbbcf1796c46cdda
nathernandez1189 <123783069+nathernandez1189@users.noreply.github.com>
2024-09-26 21:20:36 -0500

    arreglando errores

--- componentes/Cards.jsx
+++ componentes/Cards.jsx
@@ -2,46 +2,27 @@ import React from 'react';
 import './Cards.css';
 import Card from './Card'; 
 
-const Cards = () => {
-  const data = [
-    {
-      "lang": "Python",
-      "url": "https://cdn.svgporn.com/logos/python.svg",
-      "fcolor": "#00c3ff",
-      "scolor": "#ffff1c"
-    },
-    {
-      "lang": "Java",
-      "url": "https://cdn.svgporn.com/logos/java.svg",
-      "fcolor": "#D31027",
-      "scolor": "#EA384D"
-    },
-    {
-      "lang": "React",
-      "url": "https://cdn.svgporn.com/logos/react.svg",
-      "fcolor": "#000428",
-      "scolor": "#004e92"
-    },
-    {
-      "lang": "Perl",
-      "url": "https://cdn.svgporn.com/logos/perl.svg",
-      "fcolor": "#cc2b5e",
-      "scolor": "#753a88"
-    }
-  ];
+const data = [
+  {
+    lang: "YouTube Video", 
+    url: "https://www.youtube.com/embed/JS6uhdQJo4A", 
+    fcolor: "#FF0000", 
+    scolor: "#FF9900"
+  }
+];
 
+const Cards = () => {
   return (
     <div className="languages-container">
-      {data.map(({ lang, url, scolor, fcolor }) => (
-        <Card key={lang} lang={lang} img={url} fcolor={fcolor} scolor={scolor} />
+      {data.map(({ lang, url, scolor, fcolor }, index) => (
+        <Card
+          key={index} 
+          lang={lang} 
+          videoUrl={url}  
+          fcolor={fcolor}
+          scolor={scolor}
+        />
       ))}
-
-      {/* Aquí agregamos el video de YouTube */}
-      <Card 
-        isVideo={true} 
-        fcolor="#1c1c1c" 
-        scolor="#ffffff" 
-      />
     </div>
   );
 }
--- componentes/card/Card.jsx
+++ componentes/card/Card.jsx
@@ -1,34 +1,21 @@
 import React from 'react';
-import "./Card.css";
+import './Card.css';
 
-const Card = ({ lang, img, scolor, fcolor, isVideo }) => {
-    return (
-        <div 
-            className='container' 
-            style={{
-                background: `linear-gradient(to left, ${fcolor}, ${scolor})`,
-                padding: "20px",
-                borderRadius: "8px"
-            }}
-        >
-            {isVideo ? (
-                <iframe
-                    className="video-frame"
-                    src="https://www.youtube.com/embed/JS6uhdQJo4A"
-                    title="YouTube video player"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                    style={{ width: "100%", height: "315px" }}
-                ></iframe>
-            ) : (
-                <>
-                    <img src={img} alt={lang} className="language-logo" />
-                    <h2>{lang}</h2>
-                </>
-            )}
-        </div>
-    );
+const Card = ({ lang, videoUrl, fcolor, scolor }) => {
+  return (
+    <div className='container' style={{ background: `linear-gradient(45deg, ${fcolor}, ${scolor})` }}>
+      <iframe
+        width="100%" 
+        height="200" 
+        src={videoUrl}
+        title={lang}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      <h2>{lang}</h2>
+    </div>
+  );
 }
 
 export default Card;
